2e4caf69748dccea1f06e07da0fa1a6b6e8c83a9
Sławomir Śledź <slawomir.sledz@gmail.com>
2023-04-22 18:51:38 +0200

    solution ch2

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,11 @@
 module Test.MySolutions where
 
 import Prelude
+import Data.Number (pi, sqrt)
+import Data.Int (rem)
+
+diagonal x y = sqrt (x * x + y * y)
+
+circleArea r = pi * r * r
+
+leftoverCents c = c `rem` 100
